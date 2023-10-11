2b1ed148c5369a2b5b39acb977e8e5664c28b03d
KudaJay <KudaJay@gmail.com>
2023-10-11 23:28:27 +0200

    Complete and fixed import error in FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines the FileStorage class"""
 import json
-import.models.base_model import BaseModel
+from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
@@ -29,6 +29,21 @@ class FileStorage:
         ocname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
-        def save(self):
+    def save(self):
             """Serializes __objects to the JSON file __file_path"""
-            
+            odict = FileStorage.__objects
+            objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+            with open(FileStorage.__file_path, "w") as f:
+                json.dump(objdict, f)
+
+    def reload(self):
+         """Deserializes the JSON file to __objects"""
+         try:
+                with open(FileStorage.__file_path) as f:
+                    objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+            except FileNotFoundError:
+                return
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines unnitests for models_city.py"""
+
+
+import os
+import models
