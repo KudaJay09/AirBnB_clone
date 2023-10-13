5d833019eba45f1f505545334109c4a6e28ab819
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-10-13 17:50:45 +0100

    Implemented create, show, destroy and all command for the command interpreter

--- console.py
+++ console.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+This module is the command line interpreter for the clone project
+"""
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """
     This class contains the entry point for the command line interpreter
     """
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel}
 
     def do_EOF(self, line):
         """Exits the command line interpreter"""
@@ -21,6 +26,88 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """Executes when an emptyline + ENTER is an input"""
+        pass
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it to JSON file and
+        prints the id"""
+        if not line:
+            print("** class name missing **")
+        elif line not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            inst = self.classes[line]()
+            inst.save()
+            print(inst.id)
+
+    def do_show(self, line):
+        """Prints the string rep of an instance based on the class"""
+        line = line.split()
+        if not line:
+            print("** class name missing **")
+            return
+
+        if line[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(line) == 1:
+            print("** instance id missing **")
+            return
+
+        store_dict = storage.all()
+        dict_key = "{}.{}".format(line[0],line[1])
+
+        if dict_key not in store_dict:
+            print("** no instance found **")
+            return
+
+        print(store_dict[dict_key])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id, then save
+        the change into the JSON file"""
+        line = line.split()
+        if not line:
+            print("** class name missing **")
+            return
+
+        if line[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(line) == 1:
+            print("** instance id missing **")
+            return
+
+        store_dict = storage.all()
+        dict_key = "{}.{}".format(line[0],line[1])
+        if dict_key not in store_dict:
+            print("** no instance found **")
+            return
+
+        storage.all().pop(dict_key)
+        storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances based or not
+        on the class name"""
+        line = line.split()
+        if not line:
+            for value in storage.all().values():
+                print(value)
+            return
+
+        if line[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        for key, value in storage.all().items():
+            if key.split(".")[0] == line[0]:
+                print(value)
+
+    def do_update(self, line):
+        """ """
         pass
 
 if __name__ == '__main__':
